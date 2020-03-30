c881af17d6daa6d5049a2e45531df05fbcf278b4
Dmitry-B-Luckycosmetics <48524241+Dmitry-B-Luckycosmetics@users.noreply.github.com>
2020-03-30 23:37:21 +0300

    Update some logic

--- app/Services/StudyMaterialService.php
+++ app/Services/StudyMaterialService.php
@@ -8,9 +8,19 @@ use Illuminate\Database\Eloquent\Builder;
 
 class StudyMaterialService implements StudyMaterialLogicInterface
 {
+    private $response = [
+        'success' => true,
+        'data'    => [],
+        'message' => ''
+    ];
+
+    /**
+     * @param array $filter_data
+     * @return array
+     */
     public function getStudyMaterials(array $filter_data = []) : array
     {
-        $study_materials = StudyMaterial::with('categories');
+        $study_materials = StudyMaterial::with('category', 'study_material_link', 'study_material_type', 'author_type');
 
         if(!empty($filter_data)) {
             $study_materials = self::filter($study_materials, $filter_data);
@@ -19,17 +29,22 @@ class StudyMaterialService implements StudyMaterialLogicInterface
         return $study_materials->get()->toArray();
     }
 
+    /**
+     * @param Builder $query
+     * @param array $data
+     * @return Builder
+     */
     public static function filter(Builder $query, array $data) : Builder
     {
         if(!empty($data['study_material_type'])) {
             foreach($data['study_material_type'] as $smt_id) {
-                $query->where('study_material_type_id', '=', $smt_id);
+                $query->where('study_material_type_id', $smt_id);
             }
         }
 
         if(!empty($data['author_type'])) {
             foreach($data['author_type'] as $at_id) {
-                $query->where('author_type_id', '=', $at_id);
+                $query->where('author_type_id', $at_id);
             }
         }
 
@@ -44,7 +59,7 @@ class StudyMaterialService implements StudyMaterialLogicInterface
         if(!empty($data['category'])) {
             foreach($data['category'] as $category_id) {
                 $query->whereHas('categories', function (Builder $q) use($category_id) {
-                    $q->where('study_material_categories.id', '=', $category_id);
+                    $q->where('study_material_categories.id', $category_id);
                 })->get();
             }
         }
@@ -52,23 +67,59 @@ class StudyMaterialService implements StudyMaterialLogicInterface
         return $query;
     }
 
-    public function createStudyMaterial(array $data): array
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getStudyMaterial(int $id) : array
+    {
+        if($id === 0) {
+            $this->response['success'] = false;
+            $this->response['data']    = 'Не указан id учебного материала!';
+            $this->response['message'] = 'Ошибка.';
+
+            return $this->response;
+        }
+
+        $result = StudyMaterial::with('category', 'study_material_link', 'study_material_type', 'author_type')->where('id', $id)->get()->toArray();
+
+        if(empty($result)) {
+            $this->response['success'] = false;
+            $this->response['data']    = 'Учебный материал с id = ' . $id . ' не найден.';
+            $this->response['message'] = 'Ошибка.';
+        } else {
+            $this->response['data']    = $result;
+        }
+
+        return $this->response;
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function deleteStudyMaterial(int $id) : array
+    {
+        $study_material = $this->getStudyMaterial($id);
+
+        if(!$study_material['success']) {
+            return $study_material;
+        }
+
+        $affectedRows = StudyMaterial::where('id', $id)->delete();
+
+        $this->response['data'] = $affectedRows;
+
+        return $this->response;
+    }
+
+    public function createStudyMaterial(array $data) : array
     {
         // TODO: Implement createStudyMaterial() method.
     }
 
-    public function getStudyMaterial(int $id): array
-    {
-        // TODO: Implement getStudyMaterial() method.
-    }
-
-    public function updateStudyMaterial(array $data): array
+    public function updateStudyMaterial(array $data) : array
     {
         // TODO: Implement updateStudyMaterial() method.
-    }
-
-    public function deleteStudyMaterial(int $id): array
-    {
-        // TODO: Implement deleteStudyMaterial() method.
     }
 }
