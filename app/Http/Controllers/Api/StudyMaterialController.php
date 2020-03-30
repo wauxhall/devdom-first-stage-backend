ace01cd1b4a465ec38e15151e9fcf0f9290944bd
Dmitry-B-Luckycosmetics <48524241+Dmitry-B-Luckycosmetics@users.noreply.github.com>
2020-03-30 23:36:34 +0300

    Update StudyMaterialController.php

--- app/Http/Controllers/Api/StudyMaterialController.php
+++ app/Http/Controllers/Api/StudyMaterialController.php
@@ -3,41 +3,96 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StudyMaterialCreateRequest;
 use App\Http\Requests\StudyMaterialFilterRequest;
-use App\Models\StudyMaterial;
+use App\Http\Requests\StudyMaterialUpdateRequest;
 use App\Services\Contracts\StudyMaterialLogicInterface;
+use App\Traits\SendResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use function intval;
 
 class StudyMaterialController extends Controller
 {
+    use SendResponse;
 
     /**
      * @param StudyMaterialLogicInterface $studyMaterialLogic
      * @param StudyMaterialFilterRequest $request
-     * @return array
+     * @return JsonResponse
      */
-    public function index(StudyMaterialLogicInterface $studyMaterialLogic, StudyMaterialFilterRequest $request)
+    public function index(StudyMaterialLogicInterface $studyMaterialLogic, StudyMaterialFilterRequest $request) : JsonResponse
     {
-        return $studyMaterialLogic->getStudyMaterials($request->all());
+        if(!empty($request->errors)) {
+            return $this->sendError('Ошибка фильтрации данных.', $request->errors, 422);
+        }
+
+        $response = $studyMaterialLogic->getStudyMaterials($request->all());
+
+        return $this->sendResponse($response, '');
     }
 
-    public function store(Request $request)
+    /**
+     * @param StudyMaterialLogicInterface $studyMaterialLogic
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function show(StudyMaterialLogicInterface $studyMaterialLogic, Request $request) : JsonResponse
     {
-        //
+        $response = $studyMaterialLogic->getStudyMaterial(intval($request->input('study_material_id', 0)));
+
+        if(!$response['success']) {
+            return $this->sendError('Возникли ошибки.', $response['data'], 422);
+        }
+
+        return $this->sendResponse($response['data'], $response['message']);
     }
 
-    public function show(StudyMaterial $studyMaterial)
+    /**
+     * @param StudyMaterialLogicInterface $studyMaterialLogic
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function destroy(StudyMaterialLogicInterface $studyMaterialLogic, Request $request) : JsonResponse
     {
-        //
+        $response = $studyMaterialLogic->deleteStudyMaterial(intval($request->input('study_material_id', 0)));
+
+        if(!$response['success']) {
+            return $this->sendError('Возникли ошибки.', $response['data'], 422);
+        }
+
+        return $this->sendResponse($response['data'], $response['message']);
     }
 
-    public function update(Request $request, StudyMaterial $studyMaterial)
+    /**
+     * @param StudyMaterialLogicInterface $studyMaterialLogic
+     * @param StudyMaterialCreateRequest $request
+     * @return JsonResponse
+     */
+    public function store(StudyMaterialLogicInterface $studyMaterialLogic, StudyMaterialCreateRequest $request) : JsonResponse
     {
-        //
+        $response = $studyMaterialLogic->createStudyMaterial($request->all());
+
+        if(!$response['success']) {
+            return $this->sendError('Возникли ошибки.', $response['data'], 422);
+        }
+
+        return $this->sendResponse($response['data'], $response['message']);
     }
 
-    public function destroy(StudyMaterial $studyMaterial)
+    /**
+     * @param StudyMaterialLogicInterface $studyMaterialLogic
+     * @param StudyMaterialUpdateRequest $request
+     * @return JsonResponse
+     */
+    public function update(StudyMaterialLogicInterface $studyMaterialLogic, StudyMaterialUpdateRequest $request) : JsonResponse
     {
-        //
+        $response = $studyMaterialLogic->updateStudyMaterial($request->all());
+
+        if(!$response['success']) {
+            return $this->sendError('Возникли ошибки.', $response['data'], 422);
+        }
+
+        return $this->sendResponse($response['data'], $response['message']);
     }
 }
